f7664b2e4462970443d07b68ceca313f708ee22a
JadeKim042386 <jk042386@gmail.com>
2024-08-10 01:31:28 +0900

    docs: API 요청 예시를 주석으로 추가

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ class Item(BaseModel):
     name: str
     price: float
 
+"""
+GET http://localhost:9080/api/
+
+Content-Type: application/json
+{
+	"name":"joo",
+	"price": 1000
+}
+"""
 @app.get("/api/")
 def create_item(item: Item):
     # 데이터베이스에 새로운 아이템 생성 로직
